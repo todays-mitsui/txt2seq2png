1726ef29932aeb276379ee6bd60c62659069735c
三井 翔吾 <mitsui@queserser-j.co.jp>
2015-07-28 19:50:05 +0900

    gulp taskのエラーをfix.

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,22 @@
 var gulp       = require('gulp');
 
-//var browserify = require('browserify');
-//var source     = require('vinyl-source-stream');
-
 var bower      = require('main-bower-files');
 var filter     = require('gulp-filter');
 var concat     = require('gulp-concat');
 var uglify     = require('gulp-uglify');
 
 var sass       = require('gulp-ruby-sass');
+var plumber    = require('gulp-plumber');
 var csscomb    = require('gulp-csscomb');
 var please     = require('gulp-pleeease');
 
 gulp.task('default', ['bower', 'watch:sass']);
 
-/*
-gulp.task('build', function() {
-  return browserify({entries: ['./src/index.js']})
-    .bundle()
-    .pipe(source('main.js'))
-    .pipe(gulp.dest('./js/'));
-});
-*/
-
 gulp.task('bower', function() {
   return gulp.src(bower())
     .pipe(filter('**/*.js'))
     .pipe(concat('init.js'))
-    .pipe(uglify())
+    .pipe(uglify({preserveComments: 'some'}))
     .pipe(gulp.dest('./js/'));
 });
 
@@ -36,8 +25,12 @@ gulp.task('watch:sass', ['sass'], function() {
 });
 
 gulp.task('sass', function() {
-  return sass('sass/', {style: 'compact', sourcemap: false})
-//    .pipe(please({minifier: false}))
-//    .pipe(csscomb())
+  return sass('sass/', {style: 'expanded', sourcemap: false})
+    .pipe(plumber({errorHandler: function(err) {
+      console.log(err);
+    }}))
+    .pipe(filter('**/*.css'))
+    .pipe(csscomb())
+    .pipe(please())
     .pipe(gulp.dest('./css/'));
 });
